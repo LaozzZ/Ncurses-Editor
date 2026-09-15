caa775428cd8af655248a020faad9e0bd6f4d519
LaozzZ <2328721758@qq.com>
2026-09-15 23:05:00 +0800

    feat: 换行、退格实现
    实现enter键换行功能，退格键删除光标前字符功能
    fix: 方向键逻辑优化
    
    改进了方向键能够使光标到达没有字符位置的问题，现在字符、空格和换行都会被视作占位符，方向键只能使光标在有占位符的位置间移动，以使其更像一个正常的Editor

--- main.cpp
+++ main.cpp
@@ -1,44 +1,123 @@
 #include <curses.h>
 #include <ctype.h>
 
+#include <string>
+#include <vector>
+
+static void draw(const std::vector<std::string> &lines, int row, int col)
+{
+    erase();
+
+    for (int i = 0; i < (int)lines.size(); ++i)
+        mvprintw(i, 0, "%s", lines[i].c_str());
+
+    move(row, col);
+    refresh();
+}
+
 int main()
 {
     initscr();
     keypad(stdscr, TRUE);
     noecho();
 
+    std::vector<std::string> lines(1);
+    int row = 0;
+    int col = 0;
+
+    draw(lines, row, col);
+
     int ch;
 
     while ((ch = getch()) != 27)
     {
-        int y, x;
-        getyx(stdscr, y, x);
+        bool edited = false;
 
         switch (ch)
         {
         case KEY_UP:
-            move(y - 1, x);
+            if (row > 0)
+            {
+                --row;
+
+                if (col > (int)lines[row].size())
+                    col = (int)lines[row].size();
+            }
             break;
 
         case KEY_DOWN:
-            move(y + 1, x);
+            if (row + 1 < (int)lines.size())
+            {
+                ++row;
+
+                if (col > (int)lines[row].size())
+                    col = (int)lines[row].size();
+            }
             break;
 
         case KEY_LEFT:
-            move(y, x - 1);
+            if (col > 0)
+                --col;
+            else if (row > 0)
+            {
+                --row;
+                col = (int)lines[row].size();
+            }
             break;
 
         case KEY_RIGHT:
-            move(y, x + 1);
+            if (col < (int)lines[row].size())
+                ++col;
+            else if (row + 1 < (int)lines.size())
+            {
+                ++row;
+                col = 0;
+            }
+            break;
+
+        case 0x08:
+            if (col > 0)
+            {
+                lines[row].erase(col - 1, 1);
+                --col;
+                edited = true;
+            }
+            else if (row > 0)
+            {
+                col = (int)lines[row - 1].size();
+                lines[row - 1] += lines[row];
+                lines.erase(lines.begin() + row);
+                --row;
+                edited = true;
+            }
+            break;
+
+        case '\r':
+        case '\n':
+            lines.insert(lines.begin() + row + 1, lines[row].substr(col));
+            lines[row].erase(col);
+            ++row;
+            col = 0;
+            edited = true;
             break;
 
         default:
             if (isprint(ch))
-                addch(ch);
+            {
+                lines[row].insert(col, 1, (char)ch);
+                ++col;
+                edited = true;
+            }
             break;
         }
 
-        refresh();
+        if (edited)
+            draw(lines, row, col);
+        else
+        {
+            move(row, col);
+            refresh();
+        }
     }
 
     endwin();
